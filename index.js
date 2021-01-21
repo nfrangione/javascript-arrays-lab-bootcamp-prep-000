dd78bcc79d3282d01807ff4ca8f91ab438988bf4
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-21 23:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-
+var addFirs = newArray();
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
   kittens.push(name);
