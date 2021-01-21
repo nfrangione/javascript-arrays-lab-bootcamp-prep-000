d431a2ca84dbdab8ff4780e197a32c332ac0d311
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-21 23:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirsiKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKittenName(name) {
+  var addFirst = [...kittens, name];
+  return addFirst;
+}
+
+function prependKittenName(name){
+  var addFirst = [name, ...kittens];
+  return addedKittenFirst;
+}
+
+function removalLastKitten() {
+  
+}
+
+function removalFirstKitten() {
+  
+}
